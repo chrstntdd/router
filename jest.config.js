8817aaefe8f5cc592e261b6cb605c739996d682d
Christian Todd <christiantodd27@icloud.com>
2018-12-09 14:21:18 -0500

    remove useless jest config.

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,7 @@ module.exports = {
   coveragePathIgnorePatterns: ['\\.d\\.ts$'],
   coverageReporters: ['lcov'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/config/testing/file-mock.js',
-    '^.+\\.(scss|css)$': '<rootDir>/config/testing/style-mock.js',
-    '^@[/](.*)': '<rootDir>/src/client/$1'
-  },
   moduleDirectories: ['<rootDir>/src', 'node_modules'],
-  setupFiles: ['<rootDir>/config/testing/jest-setup.js'],
   testPathIgnorePatterns: [
     '<rootDir>[/\\\\](build|docs|node_modules|scripts)[/\\\\]',
     '\\.d\\.ts$'
@@ -18,7 +11,6 @@ module.exports = {
   testRegex: '\\.spec\\.(ts|tsx)$',
   testURL: 'http://localhost:3000',
   transform: {
-    '^.+\\.(scss|css)$': '<rootDir>/config/testing/style-mock.js',
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
   },
